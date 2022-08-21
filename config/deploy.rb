e0671ef7ed735cbe7b501105e265a3c73853c5cd
John Dewhurst <kinmountfair@gmail.com>
2022-08-21 10:40:21 -0400

    Added linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :branch, "main"
 append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/uploads", ".bundle"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
