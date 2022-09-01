c2759068290da263afdb9db927549cf7ed1bd6e4
John Dewhurst <kinmountfair@gmail.com>
2022-09-01 08:23:36 -0400

    Fixed in park table

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -74,6 +74,7 @@ class ReservationsController < ApplicationController
   def in_park
     # gets a list of all checked in reservations - i.e. in the park
     @in_park_reservations = Reservation.where("checked_in = ?", true).order(:lot_id)
+    # this will need to be fixed to use dates or current year
     @total_reservations = Reservation.all.size
   end
 
