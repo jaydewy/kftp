8452d82ad5fdd4fdda8f787939f43a594210d775
John Dewhurst <kinmountfair@gmail.com>
2022-11-15 23:30:52 -0500

    Added messaging to Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[ show edit update destroy ]
+
   def index
     @events = Event.order(:start_date)
   end
 
   def show
-    @event = Event.find(params[:id])
     render :edit
   end
 
@@ -15,32 +16,46 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
 
-    if @event.save
-      redirect_to events_path
-    else
-      render :new, status: :unprocessable_entity
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to events_path, notice: "Event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def edit
-    @event = Event.find(params[:id])
   end
 
   def update
-    @event = Event.find(params[:id])
-    
-    if @event.update(event_params)
-      redirect_to events_path
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to events_path, notice: "Event was successfully updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    
+    @event.destroy
+
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:name, :start_date, :end_date)
