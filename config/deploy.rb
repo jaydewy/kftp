c5fe4feb773259b54741ad87a13dbeb042a5da8b
John Dewhurst <kinmountfair@gmail.com>
2023-03-22 22:51:55 -0400

    add production config to cap shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, "main"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "config/database.yml", 'config/master.key', "config/environments/production.rb"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/uploads", ".bundle"
