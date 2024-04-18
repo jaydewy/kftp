4ae1f0069e684accaebaef8b34e0422a54b3421e
jaydewy <johnnydewhurst21@gmail.com>
2024-04-17 22:30:59 -0400

    auth - created users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_03_165139) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_17_151411) do
   create_table "campers", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "last_name", limit: 32, default: "", null: false
     t.string "first_name", limit: 32, default: "", null: false
@@ -228,6 +228,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_03_165139) do
     t.string "name", limit: 32, default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "charges", "fees"
