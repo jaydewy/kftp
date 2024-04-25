3802ba1b9b547e34561b604c69235a86c1cad161
jaydewy <johnnydewhurst21@gmail.com>
2024-04-24 23:21:07 -0400

    auth step 17

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,6 +68,21 @@ class User < ApplicationRecord
         unconfirmed? || reconfirming?
     end
 
+    def self.authenticate_by(attributes)
+        passwords, identifiers = attributes.to_h.partition do |name, value|
+          !has_attribute?(name) && has_attribute?("#{name}_digest")
+        end.map(&:to_h)
+    
+        raise ArgumentError, "One or more password arguments are required" if passwords.empty?
+        raise ArgumentError, "One or more finder arguments are required" if identifiers.empty?
+        if (record = find_by(identifiers))
+          record if passwords.count { |name, value| record.public_send(:"authenticate_#{name}", value) } == passwords.size
+        else
+          new(passwords)
+          nil
+        end
+      end
+
     private
     
     def downcase_email
